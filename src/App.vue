721ce94157b6feda9020514a6474eae3caeca498
Diego Linhares <diegomlinhares@gmail.com>
2020-09-13 02:00:45 -0300

    Logout adjustments

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,27 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link>
+      <router-link v-if="!isAuthenticated" to="/login">Login</router-link>
+      <a href="" v-if="isAuthenticated" @click.prevent="logoutUser">Logout</a>
     </div>
     <router-view/>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'App',
   computed: {
     ...mapGetters('users', ['isAuthenticated'])
+  },
+  methods: {
+    ...mapActions('users', ['logout']),
+    logoutUser() {
+      this.logout()
+          .then(() => this.$router.push('/'))
+    }
   }
 }
 </script>
